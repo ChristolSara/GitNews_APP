183126d2eb4dbcbbfcfa344971495c31c6a54b69
SaraaMiss <sara.kaouar.christol@gmail.com>
2023-05-05 15:57:42 +0200

    /:

--- app/src/main/java/fr/greta/gitnews_app/MainActivity.java
+++ app/src/main/java/fr/greta/gitnews_app/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -92,5 +93,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+//        listViewUsers.setOnClickListener(new AdapterView.OnItemClickListener()
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                String login =data.get(position)
+//            }
+//        });
+//
     }
+
 }
